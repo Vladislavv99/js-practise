6b09d4bf0b3e95654859cbc50da7662e6110600a
Vladislavv99 <vladlegur6@gmail.com>
2023-06-04 10:55:26 +0300

    .

--- src/index.js
+++ src/index.js
@@ -10,18 +10,8 @@ const usersLogins = [
 
 const BASE_URL = 'https://api.github.com';
 const END_POINT = '/users';
+const btn = document.querySelector('.btn');
 
-async function getUserAvatars(usersLogins) {
-  const userAvatars = usersLogins.map(async userName => {
-    const res = await fetch(`${BASE_URL}${END_POINT}/${userName}`);
-    return res.json();
-  });
-  return Promise.all(userAvatars);
-  //   const userAvatars = usersLogins.map(userName => {
-  //     return fetch(`${BASE_URL}${END_POINT}/${userName}`).then(res => res.json());
-  //   });
-  //   return Promise.all(userAvatars);
-}
-getUserAvatars(usersLogins).then(data => {
-  console.log(data);
-});
+btn.addEventListener('click', onBookClick);
+
+function onBookClick() {}
