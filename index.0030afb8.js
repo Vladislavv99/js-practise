b1307b7cc8c83d0eb3bde59758fcd27aa987c6fc
Vladislavv99 <124363024+Vladislavv99@users.noreply.github.com>
2023-06-04 07:55:21 +0000

    Deploying to gh-pages from @ Vladislavv99/js-practise@6b09d4bf0b3e95654859cbc50da7662e6110600a 🚀

---
+++ index.0030afb8.js
@@ -0,0 +1,2 @@
+document.querySelector(".btn").addEventListener("click",(function(){}));
+//# sourceMappingURL=index.0030afb8.js.map
---
+++ index.b5d207d8.js
@@ -0,0 +1,2 @@
+document.querySelector(".btn").addEventListener("click",(function(){}));
+//# sourceMappingURL=index.b5d207d8.js.map
